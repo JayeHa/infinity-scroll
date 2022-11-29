843848ccee2676c27e0b5e4d53028fe22515c580
정원 <jwha94@gmail.com>
2022-11-29 13:45:18 +0900

    LazyLoadImage -> Image

--- components/PokemonCard.js
+++ components/PokemonCard.js
@@ -1,18 +1,18 @@
+import Image from "next/image";
 import Link from "next/link";
-import { LazyLoadImage } from "react-lazy-load-image-component";
 
 const PokemonCard = ({ url, name }) => {
-  if (!url) return;
-
   const id = url
     .replace("https://pokeapi.co/api/v2/pokemon/", "")
     .replace("/", "");
 
   return (
     <Link href={`/pokemon/${id}`} key={id} style={{ display: "flex" }}>
-      <LazyLoadImage
+      <Image
         src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${id}.png`}
         alt={name}
+        width={100}
+        height={100}
       />
       <div>
         <div>
